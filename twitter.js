8a50745a3cc18fed147ebc78dd1e37752741c456
Poorna Chandu Gade <gadechandu123@gmail.com>
2018-03-20 07:23:20 +0530

    initial

---
+++ twitter.js
@@ -0,0 +1,62 @@
+let stream = require("stream");
+let Scraper = require("twitter-scraper").Scraper;
+let express = require("express");
+let app = require("express")();
+
+app.get("/tweets/:userId/:start/:end", (req, res) => {
+  let { userId } = req.params;
+  let { start } = req.params;
+  let { end } = req.params;
+  let query = "from:" + userId + " since:" + start + "until:" + end;
+  let scraper = new Scraper(query);
+  let tweets = {
+    data: [],
+    total: ""
+  };
+  let toConsole = new stream.Writable({
+    objectMode: true,
+    write: function(tweet, enc, cb) {
+      tweets.data.push(tweet.text);
+      // console.log("Got tweet: ", tweet.text);
+      return cb();
+    }
+  });
+  scraper.pipe(toConsole);
+  // Start the scraper
+  scraper.start();
+  toConsole.on("finish", () => {
+    tweets.total = scraper.total;
+    res.json(tweets);
+  });
+});
+
+app.get("/core-tweets/:userId/:start/:end", (req, res) => {
+  let { userId } = req.params;
+  let { start } = req.params;
+  let { end } = req.params;
+  let query = "from:" + userId + " since:" + start + "until:" + end;
+  let scraper = new Scraper(query);
+  let tweets = {
+    data: [],
+    total: ""
+  };
+  let toConsole = new stream.Writable({
+    objectMode: true,
+    write: function(tweet, enc, cb) {
+      tweets.data.push(tweet);
+      // console.log("Got tweet: ", tweet.text);
+      return cb();
+    }
+  });
+  scraper.pipe(toConsole);
+  // Start the scraper
+  scraper.start();
+  toConsole.on("finish", () => {
+    tweets.total = scraper.total;
+    res.json(tweets);
+  });
+});
+app.set("port", 3000);
+app.listen(app.get("port"), function() {
+  console.log(`server running on port ${app.get("port")}`);
+});
